a573108d9ff324dd6ac7e4b9419ae4bc07147669
Cristian Yones <cyones77@gmail.com>
2020-11-10 11:18:51 -0300

    Develop (#11)
    * Added version file
    
    * Web hook test
    
    * Extend gateway id length in db
    
    * Fix env vars check
    
    * Update VERSION
    
    * Fix/delete data collector association tables (#3)
    
    * Deleted data_collector association tables
    
    * Deleted a too verbose debugging log.
    
    * Version bump
    
    * Added session rollback when the process of saving a packet fails
    
    * Version bump
    
    * Version bump
    
    * Version bump
    
    * Disable raw message saving
    
    * Version bump
    
    * Split line
    
    * Cleaned unused code
    
    * sqlalchemy core insert to improve performance
    
    * Version bump
    
    * Version bump
    
    * Small fix
    
    * Fix, drop last message when an error occurs
    
    * Version bump
    
    * Limit the lenght of the error field
    
    * Version bump
    
    * Drop all messages when an error occur
    
    * Modified error message
    
    * Update VERSION
    
    Version bump
    
    Co-authored-by: cyones <cristianyones@argeniss.com>
    Co-authored-by: MS <MS>
    Co-authored-by: cyberms <matiassequeirq@gmail.com>

--- MQWriter.py
+++ MQWriter.py
@@ -1,13 +1,11 @@
 import atexit
 
-import boto3
-import pika, os, logging, gzip, io
+import pika, os, logging, json
+import dateutil.parser as dp
 
 from S3CollectorMessagesManager import S3CollectorMessagesManager
-from LogCollectorMessagesManager import LogCollectorMessagesManager
-from auditing.db.Models import commit, rollback, Packet
-import datetime, json
-import dateutil.parser as dp
+from auditing.db import engine, session
+from auditing.db.Models import Packet
 
 if os.environ["ENVIRONMENT"] == "DEV":
     logging.getLogger().setLevel(logging.DEBUG)
@@ -22,7 +20,6 @@ CollectorMessageManager = None
 
 
 def save_messages(messages, data_collector_id, packet_id):
-    logging.debug("Saving messages to s3")
     global CollectorMessageManager
     if CollectorMessageManager:
         # Save the message/s
@@ -31,61 +28,14 @@ def save_messages(messages, data_collector_id, packet_id):
             # if not, packet_id will be null. That's ok but we still want the property in the json
             message['packet_id'] = packet_id
         CollectorMessageManager.save_collector_messages(data_collector_id, messages)
-    else:
-        logging.info('No collector message manager available. Messages will not be saved')
 
 
-def save_packet(packet_dict):
-    logging.debug("Parsing packet")
-    # If a packet was received, persist it
-    new_packet = Packet(
-        date=dp.parse(packet_dict.get('date', None)),
-        topic=packet_dict.get('topic', None),
-        data_collector_id=packet_dict.get('data_collector_id', None),
-        organization_id=packet_dict.get('organization_id', None),
-        gateway=packet_dict.get('gateway', None),
-        tmst=packet_dict.get('tmst', None),
-        chan=packet_dict.get('chan', None),
-        rfch=packet_dict.get('rfch', None),
-        freq=packet_dict.get('freq', None),
-        stat=packet_dict.get('stat', None),
-        modu=packet_dict.get('modu', None),
-        datr=packet_dict.get('datr', None),
-        codr=packet_dict.get('codr', None),
-        lsnr=packet_dict.get('lsnr', None),
-        rssi=packet_dict.get('rssi', None),
-        size=packet_dict.get('size', None),
-        data=packet_dict.get('data', None),
-        m_type=packet_dict.get('m_type', None),
-        major=packet_dict.get('major', None),
-        mic=packet_dict.get('mic', None),
-        join_eui=packet_dict.get('join_eui', None),
-        dev_eui=packet_dict.get('dev_eui', None),
-        dev_nonce=packet_dict.get('dev_nonce', None),
-        dev_addr=packet_dict.get('dev_addr', None),
-        adr=packet_dict.get('adr', None),
-        ack=packet_dict.get('ack', None),
-        adr_ack_req=packet_dict.get('adr_ack_req', None),
-        f_pending=packet_dict.get('f_pending', None),
-        class_b=packet_dict.get('class_b', None),
-        f_count=packet_dict.get('f_count', None),
-        f_opts=packet_dict.get('f_opts', None),
-        f_port=packet_dict.get('f_port', None),
-        error=packet_dict.get('error', None),
-        latitude=packet_dict.get('latitude', None),
-        longitude=packet_dict.get('longitude', None),
-        altitude=packet_dict.get('altitude', None),
-        app_name=packet_dict.get('app_name', None),
-        dev_name=packet_dict.get('dev_name', None),
-        gw_name=packet_dict.get('gw_name', None)
-    )
-    new_packet.save_to_db()
-    logging.debug(f'Saving packet ID {new_packet.id}')
-    return new_packet
-
+BATCH_LENGHT = 64
+DATA_MAX_LEN = 300
+write_queue = []
 
 def callback(ch, method, properties, body):
-    logging.debug("Received message on queue collectors_queue")
+    global write_queue
     try:
         message = body.decode("utf-8")
         # Parse the JSON into a dict
@@ -96,48 +46,66 @@ def callback(ch, method, properties, body):
         messages = data.get('messages')
         
         if packet:
-            # Now, this packet is a DB object
-            packet= save_packet(packet)
-        else:
-            logging.debug("no packet data present")
+            if len(write_queue) < BATCH_LENGHT:
+                packet = dict(
+                    date=dp.parse(packet.get('date', None)),
+                    topic=packet.get('topic', None),
+                    data_collector_id=packet.get('data_collector_id', None),
+                    organization_id=packet.get('organization_id', None),
+                    gateway=packet.get('gateway', None),
+                    tmst=packet.get('tmst', None),
+                    chan=packet.get('chan', None),
+                    rfch=packet.get('rfch', None),
+                    freq=packet.get('freq', None),
+                    stat=packet.get('stat', None),
+                    modu=packet.get('modu', None),
+                    datr=packet.get('datr', None),
+                    codr=packet.get('codr', None),
+                    lsnr=packet.get('lsnr', None),
+                    rssi=packet.get('rssi', None),
+                    size=packet.get('size', None),
+                    data=packet['data'][0:DATA_MAX_LEN] if 'data' in packet else None,
+                    m_type=packet.get('m_type', None),
+                    major=packet.get('major', None),
+                    mic=packet.get('mic', None),
+                    join_eui=packet.get('join_eui', None),
+                    dev_eui=packet.get('dev_eui', None),
+                    dev_nonce=packet.get('dev_nonce', None),
+                    dev_addr=packet.get('dev_addr', None),
+                    adr=packet.get('adr', None),
+                    ack=packet.get('ack', None),
+                    adr_ack_req=packet.get('adr_ack_req', None),
+                    f_pending=packet.get('f_pending', None),
+                    class_b=packet.get('class_b', None),
+                    f_count=packet.get('f_count', None),
+                    f_opts=packet.get('f_opts', None),
+                    f_port=packet.get('f_port', None),
+                    error=packet['error'][0:DATA_MAX_LEN] if 'error' in packet else None,
+                    latitude=packet.get('latitude', None),
+                    longitude=packet.get('longitude', None),
+                    altitude=packet.get('altitude', None),
+                    app_name=packet.get('app_name', None),
+                    dev_name=packet.get('dev_name', None),
+                    gw_name=packet.get('gw_name', None)
+                    )
+                write_queue.append(packet)
+            else:
+                engine.execute(Packet.__table__.insert(), write_queue)
+                write_queue = []
+                session.commit()
+
         
         if messages and len(messages) > 0:
-            save_messages(messages, messages[0].get('data_collector_id'), packet.id if packet else None)
-        else:
-            logging.debug("no messages present")
+            save_messages(messages, messages[0].get('data_collector_id'), None)
     except Exception as e:
-        logging.error(f"There was an error processing packet.\n{body}\n{e}")
-        rollback()
+        logging.error(f"There was an error writing messages:\n{e}")
+        write_queue = []
+        session.rollback()
 
     try:
-        logging.debug("Acknowledging the message")
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
         logging.error("There was an error ACK-ing the packet: %s. Exception: %s" % (body, e))
-
-    try:
-        logging.debug("Committing changes to db")
-        commit()
-    except Exception as exc:
-        logging.error(f'Error at commit: {exc}')
-        logging.info('Rolling back the session')
-        rollback()
-
-
-def initialize_rabbit():
-    rabbit_credentials = pika.PlainCredentials(os.environ["RABBITMQ_DEFAULT_USER"], os.environ["RABBITMQ_DEFAULT_PASS"])
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=os.environ["RABBITMQ_HOST"],
-                                  port=int(os.environ["RABBITMQ_PORT"]),
-                                  credentials=rabbit_credentials)
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue='collectors_queue', durable=True)
-    channel.exchange_declare(exchange=os.environ["ENVIRONMENT"], exchange_type='direct')
-    channel.queue_bind(exchange=os.environ["ENVIRONMENT"], queue='collectors_queue')
-    channel.basic_consume(queue='collectors_queue', on_message_callback=callback)
-    logging.info("consuming messages on queue collectors_queue")
-    channel.start_consuming()
 
 
 def exit_handler():
@@ -145,6 +113,12 @@ def exit_handler():
     global CollectorMessageManager
     if CollectorMessageManager:
         CollectorMessageManager.flush_all()
+
+
+
+
+
+
 
 try:
     print("Starting PacketWriter")
@@ -160,22 +134,27 @@ try:
                                             logger=logging.getLogger())
     # else:
     #     CollectorMessageManager = LogCollectorMessagesManager(logger=logging.getLogger())
+
+
     print("Initializing rabbit connection")
-    initialize_rabbit()
+    rabbit_credentials = pika.PlainCredentials(os.environ["RABBITMQ_DEFAULT_USER"], os.environ["RABBITMQ_DEFAULT_PASS"])
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=os.environ["RABBITMQ_HOST"],
+                                  port=int(os.environ["RABBITMQ_PORT"]),
+                                  credentials=rabbit_credentials)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue='collectors_queue', durable=True)
+    channel.exchange_declare(exchange=os.environ["ENVIRONMENT"], exchange_type='direct')
+    channel.queue_bind(exchange=os.environ["ENVIRONMENT"], queue='collectors_queue')
+    channel.basic_consume(queue='collectors_queue', on_message_callback=callback)
+    logging.info("consuming messages on queue collectors_queue")
+    channel.start_consuming()
 except Exception as e:
     logging.error(f'There was an error initializing PacketWriter: {e}')
 finally:
     logging.info('Flushing messages to s3')
     if CollectorMessageManager:
         CollectorMessageManager.flush_all()
-
-
-# while(True):
-#     time.sleep(5)
-#     try:
-#         commit()
-#         logging.debug('Commit done!')
-#     except Exception as exc:
-#         logging.error('Error at commit:', exc)
-#         logging.info('Rolling back the session')
-#         rollback()
+    else:
+        logging.info('No collector message manager available. Messages will not be saved')
--- auditing/db/__init__.py
+++ auditing/db/__init__.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# ---> Comment these lines
 if "ENVIRONMENT" not in os.environ:
     DB_HOST = "localhost"
     DB_NAME = "loraguard_db"
@@ -11,7 +10,6 @@ if "ENVIRONMENT" not in os.environ:
     DB_PASSWORD = "postgres" 
     DB_PORT = 5432
     os.environ["ENVIRONMENT"] = "DEV"
-
 else:
     DB_HOST = os.environ["DB_HOST"] 
     DB_NAME = os.environ["DB_NAME"] 
@@ -20,44 +18,7 @@ else:
     DB_PORT = os.environ["DB_PORT"] 
 
 engine = create_engine('postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.format(user=DB_USERNAME, pw=DB_PASSWORD, url=DB_HOST, port= DB_PORT, db=DB_NAME))
-# If you'd like to use sqlite <---
-
-
-# Uncomment these lines if you want to work with sqlite instead of postgres
-# engine = create_engine('sqlite:///orm_in_detail.sqlite')
-# os.environ["ENVIRONMENT"] = "DEV"
-
 Base = declarative_base()
 sessionBuilder = sessionmaker()
 sessionBuilder.configure(bind=engine)
 session = sessionBuilder()
-
-from auditing.db.Models import AlertType, RowProcessed, rollback
-import logging
-
-if os.environ["ENVIRONMENT"] == "DEV":
-    logging.getLogger().setLevel(logging.DEBUG)
-else:
-    logging.getLogger().setLevel(logging.INFO)
-
-try:
-    if AlertType.count() == 0:
-        AlertType(code= 'LAF-001', name= 'DevNonce repeated', risk= 'LOW', description= 'DevNonces for each device should be random enough to not collide. This may be due to join message replay.').save()
-        AlertType(code= 'LAF-002', name= 'DevEUIs sharing the same DevAddr', risk= 'INFO', description= 'Two different devices might be using the same DevAddr.').save()
-        AlertType(code= 'LAF-003', name= 'Join replay', risk= 'MEDIUM', description= 'A duplicated JoinRequest message was detected. The LoRaWAN network may be under a join replay attack.').save()
-        AlertType(code= 'LAF-004', name= 'Uplink data packets replay', risk= 'MEDIUM', description= 'A duplicated uplink packet was detected.  The LoRaWAN network may be under a replay attack.').save()
-        AlertType(code= 'LAF-005', name= 'Downlink data packets replay', risk= 'HIGH', description= 'A duplicated downlink packet was detected.  The server is responding to a replay attack or is generating an atypical traffic to devices.').save()
-        AlertType(code= 'LAF-006', name= 'Counter reset and same DevAddr', risk= 'HIGH', description= "If the counter was reset (came back to 0) and the DevAddr is kept the same, this may imply that the device/server aren't regenerating the session keys. It could be also that session keys are regenerated but the server assigns the same DevAddr to the device (false positive).").save() 
-        AlertType(code= 'LAF-007', name= 'Received smaller counter for DevAddr  (distinct from 0)', risk= 'Info', description= "This may doesn't represent an attack, but it's something not likely to happen. It may be that a device is malfunctioning.").save() 
-        AlertType(code= 'LAF-009', name= 'Password cracked', risk= 'HIGH', description= 'It was possible to decrypt Join (Request and Accept) messages using a either a known or guessed AppKey.').save() 
-        AlertType(code= 'LAF-010', name= 'Gateway changed location', risk= 'MEDIUM', description= 'If the gateway is not supposed to change its location, it may have been stolen.').save()
-
-    if RowProcessed.count() == 0:
-        RowProcessed(last_row= 0, analyzer= 'bruteforcer').save_and_flush()
-        RowProcessed(last_row= 0, analyzer= 'packet_analyzer').save_and_flush()
-        RowProcessed(last_row= 0, analyzer= 'printer').save_and_flush()
-
-except Exception as exc:
-    logging.error('Error at commit when initializing:', exc)
-    logging.info('Rolling back the session')
-    rollback()
